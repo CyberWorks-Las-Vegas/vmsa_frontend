c55f3ddfe8eae26db2f2874165ef7bd2e7b3e11b
CyberWorks-Las-Vegas <oloagency@gmail.com>
2020-03-06 13:40:16 -0800

    test

--- src/Components/Pages/Dashboard/MForm/FormContainer.jsx
+++ src/Components/Pages/Dashboard/MForm/FormContainer.jsx
@@ -142,7 +142,7 @@ const FormContainer = ({ context }) => {
                     <Button
                       variant="contained"
                       color="secondary"
-                      onClick={() => { setStep(step + 1), saveContinue(e) }}
+                      onClick={(e) => { setStep(step + 1), saveContinue(e) }}
                       className={classes.button}
                     >
                       Submit
