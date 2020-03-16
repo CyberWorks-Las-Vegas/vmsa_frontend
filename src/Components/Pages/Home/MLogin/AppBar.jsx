f07ced0038174695389ed7ae7346422d21bb2854
CyberWorks-Las-Vegas <oloagency@gmail.com>
2020-03-16 12:14:08 -0700

    test

--- src/Components/Pages/Home/MLogin/AppBar.jsx
+++ src/Components/Pages/Home/MLogin/AppBar.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 
 import { withUserConsumer } from '../../../../Context/Context'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
